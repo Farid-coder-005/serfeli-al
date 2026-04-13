4eb25579773d43770b0d87bf020533dc5ecf0d5d
Ferid <zkryyevfrd1912@gmail.com>
2026-04-14 00:23:58 +0400

    style: extend logo underline to full width of Sərfəli.al

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@
      <Link href="/" className="shrink-0 flex flex-col items-start md:pr-4 group">
         <span className="text-3xl sm:text-[34px] font-black text-white tracking-tighter leading-none relative">
            Sərfəli<span className="text-[#ff5500]">.al</span>
-           <div className="absolute bottom-[-6px] left-0 w-[85%] h-[3px] bg-[#FF5500] rounded-full" />
+           <div className="absolute bottom-[-8px] left-0 w-full h-[3px] bg-[#FF5500] rounded-full" />
         </span>
      </Link>
    );
